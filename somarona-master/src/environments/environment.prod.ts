8457035438779a41bb01596ca369e7979404fec6
unknown <kwameoheneasase@gmail.com>
2021-10-05 14:13:16 +0000

    querry params

--- somarona-master/src/environments/environment.prod.ts
+++ somarona-master/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   coronaApi: 'http://api.coronastatistics.live',
-  restApi: 'https://restcountries.eu/rest/v2',
+  restApi: 'https://restcountries.eu/rest/v2?access_key=0503616cf31069c9b1f3af4a2433c327',
   ipapi:'https://ipapi.co/json/'
 };

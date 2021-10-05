f1f07a31de5834fa29da4f759bc6896df1fce158
unknown <kwameoheneasase@gmail.com>
2021-10-05 14:00:17 +0000

    added api to the environment

--- somarona-master/src/environments/environment.prod.ts
+++ somarona-master/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true,
+  coronaApi: 'http://api.coronastatistics.live',
+  restApi: 'https://restcountries.eu/rest/v2',
+  ipapi:'https://ipapi.co/json/'
 };
